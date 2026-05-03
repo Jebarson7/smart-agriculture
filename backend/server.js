1d2b7bd81b520132fd82955d12838feb55fa63bc
Jebarson7 <jebarson.e2024cce@sece.ac.in>
2026-05-03 20:09:08 +0530

    update backend routes

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 
 const app = express();
+const cors = require("cors");
+
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: [
+    "http://localhost:3000",
+    "https://smart-agriculture-theta.vercel.app"
+  ],
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"]
 }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Smart Agriculture Backend is Running 🌱");
+});
+
 // 🔗 Connect MongoDB (we will update later)
-mongoose.connect("mongodb+srv://admin:admin123@cluster0.zghtwbo.mongodb.net/agriculture?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => console.log("MongoDB Connected"))
+mongoose.connect("mongodb+srv://admin:admin123@cluster0.zghtwbo.mongodb.net/agriculture?retryWrites=true&w=majority")
+.then(() => {
+  console.log("MongoDB Connected");
+
+  app.listen(5000, () => {
+    console.log("Server running on port 5000");
+  });
+
+})
 .catch(err => console.log(err));
 
 // Routes
@@ -23,6 +38,3 @@ const irrigationRoutes = require("./routes/irrigationRoutes");
 app.use("/api/irrigation", irrigationRoutes);
 
 // Start server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
